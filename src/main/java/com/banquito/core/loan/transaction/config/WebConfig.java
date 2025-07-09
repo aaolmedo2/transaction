0c9ce058b950a85fab60efd80d55a80ef7ce363c
Angelo Andres Olmedo Camacho <aaolmedo2@espe.edu.ec>
2025-07-09 01:28:40 -0500

    CORS

--- src/main/java/com/banquito/core/loan/transaction/config/WebConfig.java
+++ src/main/java/com/banquito/core/loan/transaction/config/WebConfig.java
@@ -1,21 +1,17 @@
 package com.banquito.core.loan.transaction.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
-    @Value("${frontend.url}")
-    private String frontendUrl;
-
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins(frontendUrl)
+        registry.addMapping("/api/")
+                .allowedOrigins("http://ec2-18-116-67-249.us-east-2.compute.amazonaws.com:4174")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*");
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
